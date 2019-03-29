b0c793d719d49644dfbd3cfd49e2074b5b5f5fef
Max Hsu <maxhsu1@gmail.com>
2019-03-28 17:07:57 -0700

    completed getallreviews function to query database

---
+++ database/index.js
@@ -0,0 +1,23 @@
+var mysql = require('mysql');
+var con = mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  database:'reviewsdb'
+})
+
+con.connect(()=>{console.log('DATABASEHELPERS.JS LINE 8 connceted to db')})
+
+var getAllReviews = (callback) => {
+
+  con.query(`select * from reviews`,(err,result)=>{
+    if (err) {
+      callback(err,null)
+    } else {
+      callback(null,result)
+    }
+  })
+}
+
+module.exports = {
+  getAllReviews:getAllReviews
+}
