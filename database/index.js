2bca40acf187b0def8c5fa3526c8c8ada918946b
Max Hsu <maxhsu1@gmail.com>
2019-03-29 16:00:54 -0700

    removed date insertion here as date is created upon insertion in db

--- database/index.js
+++ database/index.js
@@ -18,6 +18,30 @@ var getAllReviews = (productid, callback) => {
   })
 }
 
+var postReview = (productid, formBody, callback) => {
+  con.query(`insert into reviews (username, rating, size, comfort, prod_durability, review_title, review_text, country, city,loc_state, usefor,productid ) values (?,?,?,?,?,?,?,?,?,?,?,?,?)`,
+  [
+    req.body.username,
+    req.body.rating,
+    req.body.size,
+    req.body.comfort,
+    req.body.prod_durability,
+    req.body.review_title,
+    req.body.review_text,
+    req.body.country,
+    req.body.city,
+    req.body.loc_state,
+    req.body.userfor,
+    productid
+  ],(err,result) => {
+    if (err) {
+      callback(err, null)
+    } else {
+      callback(null,result)
+    }
+  })
+}
+
 module.exports = {
   getAllReviews:getAllReviews
 }
