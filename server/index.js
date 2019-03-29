3ade3c7813be9b01041db1519b67fbfa7e0e0af0
Max Hsu <maxhsu1@gmail.com>
2019-03-28 17:06:25 -0700

    completed get request for all reviews from db

---
+++ server/index.js
@@ -0,0 +1,31 @@
+var con = require('../database')
+var mysql = require('mysql');
+var express = require('express');
+var bodyParser = require ('body-parser')
+var app = express()
+
+app.get('/api/reviews',(req,res)=>{
+
+  con.getAllReviews((err,result)=>{
+    if(err) {
+      console.log('ERROR HERE',err)
+    } else {
+      res.status(200).json(result)
+    }
+  })
+
+
+})
+
+
+var port = 7000
+app.listen(port,(err,result)=>{
+  if(err){
+    console.log('SERVER.JS LINE 10 ERROR')
+  } else {
+    console.log(`SERVER.JS LINE 13 LISTENING ON ${port}`)
+  }
+}
+)
+
+
