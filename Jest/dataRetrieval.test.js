9a5e382e594e5df2aa22730624cebb45c3b310c9
Max Hsu <maxhsu1@gmail.com>
2019-03-29 14:24:45 -0700

    added Jest test for data retrieval

--- Jest/dataRetrieval.test.js
+++ Jest/dataRetrieval.test.js
@@ -1,1 +1,24 @@
-var test = require('../database/index')
+//var mysql = require('mysql');
+var con = require('../database/index')
+
+test('Get all initial data from reviews', done => {
+  function callback(data){
+    expect(data.length).toBe(20);
+
+    done()
+  }
+  con.getAllReviews((err,result)=>{
+    if(err){callback(err)}
+    else {
+      callback(result)
+    }
+  })
+
+})
+
+
+
+
+
+
+
