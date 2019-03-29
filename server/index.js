639898e469fc58ed0cf05306232c3b03416cf284
Max Hsu <maxhsu1@gmail.com>
2019-03-29 15:35:11 -0700

    added post functionality

--- server/index.js
+++ server/index.js
@@ -2,8 +2,14 @@ var con = require('../database')
 var mysql = require('mysql');
 var express = require('express');
 var bodyParser = require ('body-parser')
+var multer = require('multer')
 var app = express()
+var upload = multer()
 
+
+
+
+//GET ALL REVIEWS FROM DB BASED ON PROD ID
 app.get('/api/reviews/:productid',(req,res)=>{
 var productid = req.params.productid
 
@@ -14,9 +20,14 @@ var productid = req.params.productid
       res.status(200).json(result)
     }
   })
-
-
 })
+
+//SUBMIT REVIEW
+app.post('/api/reviews/:productid',upload.none(),(req,res)=>{
+  res.status(200).json(req.body)
+  console.log('POST RECEIVED', req.body)
+})
+
 
 
 var port = 7000
