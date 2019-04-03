2337dc74728c80242977cdb750d821f4440af3bd
Max Hsu <maxhsu1@gmail.com>
2019-04-03 12:32:51 -0700

    basic layout completed

--- client/src/components/reviews.jsx
+++ client/src/components/reviews.jsx
@@ -12,7 +12,19 @@ var Reviews = (props) =>(
     <Oneframe>durability</Oneframe>
   </InfoPanel>
 {props.allreviews.map((i,index)=>
-  <div>{i.review_id}</div>)}
+  <ReviewPanel>
+    <ReviewInfo>
+    <Starframe>test</Starframe>
+    <SizeFrame>test</SizeFrame>
+    <ComfFrame>test</ComfFrame>
+    <DurFrame>test</DurFrame>
+    </ReviewInfo>
+    <BodyFrame>
+      <BodyTitle>{i.review_title}</BodyTitle>
+      <BodyText>{i.review_text}</BodyText>
+      <BodyDetails>{i.date_submitted}</BodyDetails>
+    </BodyFrame>
+  </ReviewPanel>)}
 
 
 </div>
@@ -33,6 +45,60 @@ grid-template-columns: auto auto auto
 var Oneframe = styled.span`
 font-size:14px;
 font-family:'Helvetica Neue';
+padding-bottom:10px;
+`
+
+var ReviewPanel = styled.div`
+display:grid
+grid-template-columns:200px 500px
+border-bottom: 0.5px solid #d6d5d5;
+`
+
+var ReviewInfo = styled.div`
+grid-column:1/2;
+padding-bottom: 10px;
+padding-top:10px;
+`
+
+var Starframe = styled.div`
+grid-area:star;
+background:black;
+height:40px;
+`
+var SizeFrame = styled.div`
+grid-area: size;
+background:blue;
+height:40px;
+`
+var ComfFrame = styled.div`
+grid-area: comfort;
+background:orange;
+height:40px;
+`
+var DurFrame = styled.div`
+grid-area: dur;
+background:red;
+height:40px;
+`
+var BodyFrame = styled.div`
+grid-column:2/3
+font-family:'Helvetica Neue';
+`
+var BodyTitle = styled.div`
+padding-top:10px;
+font-weight: bold;
+font-family:'Helvetica Neue';
+`
+var BodyText = styled.div`
+
+font-family:'Helvetica Neue';
+font-size:12px;
+`
+
+var BodyDetails = styled.div`
+font-family:'Helvetica Neue';
+font-size:12px;
+padding-top:10px
 `
 
 export default Reviews;
