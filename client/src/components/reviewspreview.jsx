d26a40c4f2f14b7bf199433dd9ed9523be47ef48
Max Hsu <maxhsu1@gmail.com>
2019-04-02 17:54:03 -0700

    completed styling, still need to add stars

--- client/src/components/reviewspreview.jsx
+++ client/src/components/reviewspreview.jsx
@@ -1,17 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
 
 var ReviewsPreview = (props) =>(
   <div>
    {props.allreviews.slice(0,3).map((i)=>
-    <div>
-      <div>{i.review_title}</div>
-      <div>{i.rating}</div>
-      <div>{i.username}</div>
-      <div>{i.current_timestamp}</div>
-      <div>{i.review_text}</div>
-    </div>)}
+    <Eachreviewdiv>
+      <Titlediv>{i.review_title}</Titlediv>
+      <Starspan>{i.rating}</Starspan>
+      <Userspan>{i.username}</Userspan>
+      <Textbodydiv>{i.current_timestamp}</Textbodydiv>
+      <Textbodydiv>{i.review_text}</Textbodydiv>
+    </Eachreviewdiv>)}
   </div>
 )
+
+//STYLED COMPONENTS
+var Starspan = styled.span`
+font-size:12px;
+padding-right:5px;
+`
+var Userspan = styled.span`
+font-size:12px
+`
+var Textbodydiv = styled.div`
+font-size:12px;
+font-family:'Helvetica Neue'
+`
+
+var Titlediv = styled.div`
+font-size:12px;
+font-family:'Helvetica Neue';
+font-weight:bold;
+`
+var Eachreviewdiv = styled.div`
+padding-top:12px;
+padding-bottom:5px;
+`
+
 
 
 export default ReviewsPreview
