82023ab61596c3116a59ea9348e7b1aa79200088
Max Hsu <maxhsu1@gmail.com>
2019-04-12 23:50:36 -0700

    added star rating in form

--- client/src/components/Modal.writereview.jsx
+++ client/src/components/Modal.writereview.jsx
@@ -66,12 +66,22 @@ class InputForm extends React.Component{
         </Divcol>
 
         <Divtext>
-            <input type = 'radio' name ='rating' value = '1'></input>
-            <input type = 'radio' name ='rating' value = '2'></input>
-            <input type = 'radio' name ='rating' value = '3'></input>
-            <input type = 'radio' name ='rating' value = '4'></input>
-            <input type = 'radio' name ='rating' value = '5'></input>
+          <span className="star-cb-group">
 
+
+<input type="radio" id="rating-5" name="rating" value="5" />
+      <label for="rating-5" >5</label>
+      <input type="radio" id="rating-4" name="rating" value="4"  />
+      <label for="rating-4">4</label>
+      <input type="radio" id="rating-3" name="rating" value="3" />
+      <label for="rating-3">3</label>
+      <input type="radio" id="rating-2" name="rating" value="2" />
+      <label for="rating-2">2</label>
+      <input type="radio" id="rating-1" name="rating" value="1" />
+      <label for="rating-1">1</label>
+      <input type="radio" id="rating-0" name="rating" value="0" class="star-cb-clear" />
+      <label for="rating-0">0</label>
+      </span>
             </Divtext>
 
 
@@ -246,8 +256,9 @@ color:white;
 border-radius:2px;
 font-size:14px;
 font-family:'Helvetica Neue';
-
 `
+
+
 
 
 
