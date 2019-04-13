77467945c90e80738a92062caa0bfbd8bc729491
Max Hsu <maxhsu1@gmail.com>
2019-04-12 23:50:21 -0700

    created star rating helper function

--- client/src/components/Starhelper.jsx
+++ client/src/components/Starhelper.jsx
@@ -5,21 +5,21 @@ function StarHelper(props){
     return <span>&#9733;&#9733;&#9733;&#9733;&#9733;</span>
   }
   if (props.rating===4) {
-    return <span>&#9733;&#9733;&#9733;&#9733;</span>
+    return <span>&#9733;&#9733;&#9733;&#9733;&#9734;&#9734;</span>
   }
   if (props.rating===3) {
-    return <span>&#9733;&#9733;&#9733;</span>
+    return <span>&#9733;&#9733;&#9733;&#9734;&#9734;</span>
   }
   if (props.rating===2) {
-    return <span>&#9733;&#9733;</span>
+    return <span>&#9733;&#9733;&#9734;&#9734;&#9734;</span>
   }
   if (props.rating===1) {
-    return <span>&#9733;</span>
+    return <span>&#9733;&#9734;&#9734;&#9734;&#9734;</span>
   }
 
 
   return (
-    <span>&#9733;</span>
+    <span>&#9733;&#9734;&#9734;&#9734;&#9734;</span>
   );
 }
 
